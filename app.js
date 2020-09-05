e8d8afa568b864811b4a6d8c95b007cbae33eaa2
Mohammed Abahussain <Mohammed@MacBook-Air-alkhas-b-Mohammed.local>
2020-09-06 00:31:41 +0300

    Fixed gitignore

--- app.js
+++ app.js
@@ -26,8 +26,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-const secret = "this is a secret";
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 
 const User = mongoose.model("User", userSchema);
