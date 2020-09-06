d2c0dc73268ea9ab13f75807c690ba841d92521f
Mohammed Abahussain <Mohammed@MacBook-Air-alkhas-b-Mohammed.local>
2020-09-06 22:56:27 +0300

    First commit GitHub log in is not added yet

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 
 const app = express();
@@ -15,77 +19,131 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(session({
+      secret: "our secret",
+      resave: false,
+      saveUninitialized: false
+    }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    });
+mongoose.set('useCreateIndex', true);
+
+    const userSchema = new mongoose.Schema({
+      email: String,
+      password: String,
+      googleId: String,
+      secret: String
+    });
+
+    userSchema.plugin(passportLocalMongoose);
+    userSchema.plugin(findOrCreate);
+
+    const User = mongoose.model("User", userSchema);
+
+    passport.use(User.createStrategy());
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
 
-const userSchema = new mongoose.Schema ({
-  email: String,
-  password: String
-});
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
-
-
-const User = mongoose.model("User", userSchema);
-
-
-app.get("/", function(req, res) {
-  res.render("home");
-})
-
-app.get("/login", function(req, res) {
-  res.render("login");
-})
-
-app.get("/register", function(req, res) {
-  res.render("register");
-})
-
-app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  })
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
   });
-})
+});
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLINET_ID,
+  clientSecret: process.env.CLINET_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/secrets",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+console.log(profile);
+User.findOrCreate({ googleId: profile.id }, function (err, user) {
+  return cb(err, user);
+});
+}
+));
 
+    app.get("/", function(req, res) {
+      res.render("home");
+    })
 
-  User.findOne({
-      email: username
-    },
-    function(err, foundUser) {
-      if (!err) {
-        if (!foundUser) {
-          res.send("Wrong Email or password");
-        } else {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          } else {
-            res.send("Wrong Email or password");
-          }
-        }
-      } else {
-        res.send(err);
+    app.get("/auth/google",
+      passport.authenticate("google", { scope: ["profile"] }
+    ));
+
+    app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login"}),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
+
+    app.get("/login", function(req, res) {
+      res.render("login");
+    })
+
+    app.get("/register", function(req, res) {
+      res.render("register");
+    })
+
+    app.get("/secrets", function(req, res){
+      if(req.isAuthenticated()){
+        res.render("secrets");
+      }else{
+        res.render("login");
       }
-    }
-  )
-})
+    })
+
+    app.get("/logout", function(req, res){
+      req.logout();
+      res.redirect("/");
+    })
+
+    app.post("/register", function(req, res) {
+
+      User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+          console.log(err);
+          res.redirect("register")
+        }else{
+          passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets")
+          })
+        }
+      })
+    })
+
+    app.post("/login", function(req, res) {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password
+      });
+
+      req.login(user, function(err){
+        if(err){
+          console.log(err);
+        }else{
+          passport.authenticate("local")(req, res, function(){
+            res.redirect("/secrets")
+          })
+        }
+      })
+
+    })
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-})
+    app.listen(3000, function() {
+      console.log("Server started on port 3000");
+    })
